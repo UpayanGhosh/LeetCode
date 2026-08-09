957822c508db64f7309a31e9ba908ae79e3359ac
Upayan Ghosh <86832133+UpayanGhosh@users.noreply.github.com>
2026-08-10 02:34:46 +0530

    Create 605-CanPlaceFlowers

---
+++ 605-CanPlaceFlowers/605-CanPlaceFlowers.py
@@ -0,0 +1,12 @@
+# Last updated: 10/08/2026, 02:34:45
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        if n == 0:
+            return True
+        for i in range(len(flowerbed)):
+            if flowerbed[i] == 0 and (i == 0 or flowerbed[i-1] == 0) and (i == len(flowerbed)-1 or flowerbed[i+1] == 0):
+                flowerbed[i] = 1
+                n -= 1
+                if n == 0:
+                    return True
+        return False
