47b105ee840ed29e510e384785463c73f345af26
Upayan Ghosh <86832133+UpayanGhosh@users.noreply.github.com>
2026-08-10 02:35:29 +0530

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/283-MoveZeroes.py
@@ -0,0 +1,10 @@
+# Last updated: 10/08/2026, 02:35:28
+class Solution(object):
+    def moveZeroes(self, nums):
+        j = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                j+=1
+
+        
