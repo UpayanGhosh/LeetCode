fe4c34ddd717de7079acada5e5dbf6f961501c91
Upayan Ghosh <86832133+UpayanGhosh@users.noreply.github.com>
2026-08-10 02:36:05 +0530

    Create 151-ReverseWordsInAString

---
+++ 151-ReverseWordsInAString/151-ReverseWordsInAString.py
@@ -0,0 +1,15 @@
+# Last updated: 10/08/2026, 02:36:04
+class Solution(object):
+    def reverseWords(self, s):
+        words = s.split()
+        return ' '.join(reversed(words))
+        
+
+        
+            
+        
+
+
+
+        
+        
