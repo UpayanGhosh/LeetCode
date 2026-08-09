14798e9d73476a6fc7bb2c98344963ea1b811e89
Upayan Ghosh <86832133+UpayanGhosh@users.noreply.github.com>
2026-08-10 02:34:12 +0530

    Create 1078-RemoveOutermostParentheses

---
+++ 1078-RemoveOutermostParentheses/1078-RemoveOutermostParentheses.cpp
@@ -0,0 +1,22 @@
+// Last updated: 10/08/2026, 02:34:11
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string result;
+        int depth = 0;
+        for (char c : s) {
+            if (c == '(') {
+                if (depth > 0) {
+                    result.push_back(c);
+                }
+                ++depth;
+            } else { // c == ')'
+                --depth;
+                if (depth > 0) {
+                    result.push_back(c);
+                }
+            }
+        }
+        return result;
+    }
+};
